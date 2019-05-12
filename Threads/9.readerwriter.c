32af090882e2ff305b3eeba844ce26897aadd3a1
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-05-12 10:49:35 +0530

    fixes

--- Threads/9.readerwriter.c
+++ Threads/9.readerwriter.c
@@ -13,15 +13,15 @@
 #include <unistd.h>
 
 pthread_t tId;
-pthread_mutex_t readState, writeState;
+pthread_mutex_t readMutex, writeMutex;
 
 int readerCount, writerCount;
 
 void initialize()
 {
     // initialize the mutex
-    pthread_mutex_init(&readState, NULL);
-    pthread_mutex_init(&writeState, NULL);
+    pthread_mutex_init(&readMutex, NULL);
+    pthread_mutex_init(&writeMutex, NULL);
     readerCount = writerCount = 0;
 }
 
@@ -33,7 +33,7 @@ void *reader(void *args)
     printf("\n[R] : Reader is trying to enter");
 
     // set read mutex to locked state
-    pthread_mutex_lock(&readState);
+    pthread_mutex_lock(&readMutex);
 
     // increment read count
     ++readerCount;
@@ -41,12 +41,12 @@ void *reader(void *args)
     // give priority to reader on the first try by locking write mutex
     if (readerCount == 1)
     {
-        pthread_mutex_lock(&writeState);
+        pthread_mutex_lock(&writeMutex);
     }
     printf("\n--->  %d Reader(s) inside ", readerCount);
 
     // unlock reader mutex for other readers
-    pthread_mutex_unlock(&readState);
+    pthread_mutex_unlock(&readMutex);
     sleep(waitTime);
 
     printf("\n[R] : Reader %d is Leaving", readerCount);
@@ -54,7 +54,7 @@ void *reader(void *args)
     // if all readers left, unlock write mutex
     if (readerCount == 0)
     {
-        pthread_mutex_unlock(&writeState);
+        pthread_mutex_unlock(&writeMutex);
     }
 }
 
@@ -65,14 +65,14 @@ void *writer(void *args)
 
     printf("\n[W] : Writer is trying to enter");
     // lock write mutex
-    pthread_mutex_lock(&writeState);
+    pthread_mutex_lock(&writeMutex);
 
     ++writerCount;
     printf("\n--->  Writer %d has entered", writerCount);
 
     sleep(waitTime);
     // unlock write mutex
-    pthread_mutex_unlock(&writeState);
+    pthread_mutex_unlock(&writeMutex);
 
     printf("\n[W] : Writer %d is leaving", writerCount);
     sleep(15);
